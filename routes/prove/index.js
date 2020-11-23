7ff75d455109e49f3513a7ca8ad17d7d4e5589f3
Leonardo <leosud099@gmail.com>
2020-11-23 13:08:40 -0700

    Added new route

--- routes/prove/index.js
+++ routes/prove/index.js
@@ -4,10 +4,12 @@ const prove = require("express").Router();
 const prove01Routes = require("../../routes/prove/prove01/prove01");
 const prove02Data = require("../../routes/prove/prove02/prove02Input");
 const prove02 = require("../../routes/prove/prove02/prove02");
+const prove10 = require("../prove/prove10/prove10");
 
 prove
   .use("/prove01", prove01Routes)
   .use("/prove02Input", prove02Data.routes)
-  .use("/prove02", prove02);
+  .use("/prove02", prove02)
+  .use("/prove10", prove10);
 
 module.exports = prove;
